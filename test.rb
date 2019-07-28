af583df5cc03628f55441c1c27e1bd3924ac89cb
ecshopcat <53403862+ecshopcat@users.noreply.github.com>
2019-07-28 23:40:23 +0800

    Add files via upload

---
+++ test.rb
@@ -0,0 +1,25 @@
+
+var1 = "stop"
+var2 = "aGGGa"
+var3 = var1.reverse
+variable = 5
+variable2 = "2"
+
+puts var1.reverse
+puts var1.length
+puts var1.upcase
+puts var2.downcase
+
+puts "I have #{var3} at the #{var2}!!!!!!!" + " string ins"
+
+puts variable.to_s + variable2
+puts variable + variable2.to_f
+
+puts "======================================"
+ text = ["eve" , " James","mom"]
+ text.each do |text|
+ puts "I LOVE " + text +"!"
+ end
+ 
+
+ 
